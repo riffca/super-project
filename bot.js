5b3dc2e587599a5c225141991cfd71b264c48ad6
riffca <riffca@yandex.ru>
2017-06-15 03:03:36 +0300

    change bot

--- bot.js
+++ bot.js
@@ -5,17 +5,51 @@ var app = express();
 /* serves main page */
 app.get("/build-client", function(req, res) {
   const spawn = require( 'child_process' ).spawn
-  ls = spawn(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ['run',  'build']);
-  ls.stdout.on( 'data', data => {
-      console.log( `stdout: ${data}` );
-  });
-  ls.stderr.on( 'data', data => {
-      console.log( `stderr: ${data}` );
-  });
-  ls.on( 'close', code => {
-      console.log( `child process exited with code ${code}` );
-      res.send(`stderr: ${code}` )
-  });
+
+
+
+  Promise.resolve().then(()=>{
+
+    let dat
+
+
+    ls = spawn(/^win/.test(process.platform) ? 'git.cmd' : 'git', ['pull']);
+    ls.stdout.on( 'data', data => {
+        dat = data
+        console.log( `stdout: ${data}` );
+    });
+    ls.stderr.on( 'data', data => {
+        dat = data
+        console.log( `stderr: ${data}` );
+    });
+    ls.on( 'close', code => {
+        console.log( `child process exited with code ${code}` );
+        res.send(`stderr: ${code}` )
+    });
+
+
+  }).then(()=>{
+
+    ls = spawn(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ['run',  'build']);
+    ls.stdout.on( 'data', data => {
+        dat = data
+        console.log( `stdout: ${data}` );
+    });
+    ls.stderr.on( 'data', data => {
+        dat = data
+        console.log( `stderr: ${data}` );
+    });
+    ls.on( 'close', code => {
+        console.log( `child process exited with code ${code}` );
+        res.send(`stderr: ${code}` )
+    });
+
+
+  })
+
+
+
+
 });
 
 var port = process.env.PORT || 5000;
