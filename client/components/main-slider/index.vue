e9d69954e12908a99829b97ed2309d7543c1ee00
riffca <riffca@yandex.ru>
2017-06-23 00:54:28 +0300

    fix mobile

--- client/components/main-slider/index.vue
+++ client/components/main-slider/index.vue
@@ -4,14 +4,15 @@
     .fake
     img(src="./img/flash.png" v-if="flash")
     img(src="./img/no-flash.png" v-if="!flash")
-
     .paper
       img(src="./img/list.png")
 
-
-
-
-
+  .load-before
+    img(src="./img/flash.png")
+    img(src="./img/no-flash.png")
+    img(src="./img/pink_bg.jpg")
+    img(src="./img/grey_bg.jpg")
+    img(src="./img/purpl_bg.jpg")
 
 
 </template>
@@ -68,6 +69,10 @@ export default {
       transform: translateX(-35px);
       display: inline-block;
       max-width: 400px;
+
+      @media screen and (max-width: 750px){
+        transform: translateX(0px);
+      }
     }
   }
 
@@ -77,6 +82,15 @@ export default {
     top: 50px;
     right: -40px;
     img {width: 300px}
+
+    @media screen and (max-width: 750px){
+      display: none;
+    }
+  }
+
+  .load-before {
+    position: abslute;
+    right: -5000px;
   }
 
 }
